928e773961a507b0bb830812b2cfd1289b1af52d
mystified131 <mystifiedthomas@gmail.com>
2020-06-28 18:19:56 -0500

    Tweaking

---
+++ TotalPlaylistGenerator.py
@@ -0,0 +1,44 @@
+#This code imports the necessary modules.
+
+import random
+import os
+import datetime
+
+right_now = datetime.datetime.now().isoformat()
+list = []
+
+for i in right_now:
+    if i.isnumeric():
+        list.append(i)
+
+tim = ("".join(list))
+
+oustr = "TotalPlayList" + tim + ".m3u"
+
+srchstr = 'H:\\OriginalAudio\\Songs'
+
+outfile = open(oustr, "w")
+
+print("")
+
+for subdir, dirs, files in os.walk(srchstr):
+    for file in files:
+        filepath = subdir + os.sep + file
+
+        if filepath.endswith(".mp3") or filepath.endswith(".wav") or filepath.endswith(".ogg") :
+            outfile.write(filepath + '\n')
+            print(filepath)
+
+outfile.close()
+
+print("")
+
+print("Your playlist may be found in the same folder as this code.")
+
+
+
+
+
+
+
+## THE GHOST OF THE SHADOW ##
